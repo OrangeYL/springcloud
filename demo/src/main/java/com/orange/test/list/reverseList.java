2ff7758ced3b91eeef63cec8ea0094a52df3401c
orange <orange@qq.com>
2021-10-14 11:32:53 +0800

    新加的demo

--- demo/src/main/java/com/orange/test/list/reverseList.java
+++ demo/src/main/java/com/orange/test/list/reverseList.java
@@ -7,28 +7,37 @@ public class reverseList {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n=scanner.nextInt();
-        ListNode head = new ListNode(0);
-        ListNode tem=head;
+        ListNode temp1 = new ListNode(0);
+        ListNode temp2=temp1;
         while(n>0){
-            tem = new ListNode(scanner.nextInt());
-
+            temp2.next= new ListNode(scanner.nextInt());
+            temp2=temp2.next;
+            n--;
         }
+       ListNode head=temp1.next;
+        ListNode node = reverse(head);
+        while(node!=null){
+           System.out.print(node.val+"  ");
+           node=node.next;
+       }
     }
-
-
-    public ListNode reverse(ListNode head){
-
-
-        return null;
+    public static ListNode reverse(ListNode head){
+        ListNode pre=null;
+        ListNode cur=head;
+        ListNode nex=null;
+        while(cur!=null){
+            nex=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=nex;
+        }
+        return pre;
     }
 }
 
-
-
-
 class ListNode{
-    private ListNode next;
-    private int val;
+    public  ListNode next;
+    public  int val;
 
     public ListNode(int val) {
         this.val = val;
